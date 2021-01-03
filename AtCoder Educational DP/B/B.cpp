eba2f24ad1cce33ee7a3a408228712483eaa2d0f
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-01-03 15:57:38 -0500

    Update B.cpp

--- AtCoder Educational DP/B/B.cpp
+++ AtCoder Educational DP/B/B.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, K;
+	cin >> N >> K;
+	vector<int> h(N);
+	for(int&x : h) cin >> x;
+	
+	vector<int> dp(N, INT_MAX);
+	dp[0] = 0;
+	
+	for(int i = 1; i < N; ++i) {
+		for(int j = 1; j <= min(i, K); ++j) {
+			dp[i] = min(dp[i], dp[i-j] + abs(h[i] - h[i-j]));
+		}
+	}
+	
+	cout << dp[N-1] << '\n';
+	return 0;
+}
