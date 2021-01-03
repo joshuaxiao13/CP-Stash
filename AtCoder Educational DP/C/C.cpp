0b7ad09af82f3a4c4f2354bd9d2898c6c62e642a
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-01-03 16:36:39 -0500

    Update C.cpp

--- AtCoder Educational DP/C/C.cpp
+++ AtCoder Educational DP/C/C.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	
+	vector<vector<int>> happiness(3, vector<int>(N));
+	
+	for(int day = 0; day < N; ++day)
+		for(int i = 0; i < 3; ++i)
+			cin >> happiness[i][day];
+	
+	vector<vector<int>> dp(N, vector<int>(3, 0));
+	
+	for(int i = 0; i < 3; ++i)
+		dp[0][i] = happiness[i][0];
+	
+	for(int day = 1; day < N; ++day) {
+		for(int i = 0; i < 3; ++i) {
+			for(int j = 0; j < 3; ++j) {
+				if(j == i) continue;
+				dp[day][i] = max(dp[day][i], dp[day-1][j] + happiness[i][day]);
+			}
+		}
+	}
+	
+	cout << max({dp[N-1][0], dp[N-1][1], dp[N-1][2]})<< '\n';
+	
+	return 0;
+}
 
