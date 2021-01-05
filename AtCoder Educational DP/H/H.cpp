87306f9e2e275899b6f21a993999bc4ee2cfaebb
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-01-05 13:22:42 -0500

    Create H.cpp

---
+++ AtCoder Educational DP/H/H.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int H, W;
+	cin >> H >> W;
+	
+	vector<vector<bool>> grid(H, vector<bool>(W));
+	
+	for(int j = 0; j < H; ++j) {
+		for(int i = 0; i < W; ++i) {
+			char a;
+			cin >> a;
+			grid[j][i] = (a == 	'.');
+		}
+	}
+	
+	vector<vector<int>> dp(H, vector<int>(W, 0));
+	
+	dp[0][0] = 1;
+	
+	for(int j = 0; j < H; ++j) {
+		for(int i = 0; i < W; ++i) {
+			if(j - 1 >= 0 && grid[j-1][i]) dp[j][i] = (dp[j][i] + dp[j-1][i]) % MOD;
+			if(i - 1 >= 0 && grid[j][i-1])  dp[j][i] = (dp[j][i] + dp[j][i-1]) % MOD;
+		}
+	}
+	
+	cout << dp[H-1][W-1] << '\n';
+	
+	return 0;
+}
