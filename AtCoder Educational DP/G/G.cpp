ca5377dd33e99d8546f3c126f22c216b023dc1b7
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-01-05 12:19:03 -0500

    Create G.cpp

---
+++ AtCoder Educational DP/G/G.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mxN = 1e5;
+
+vector<int> adj[mxN];
+bool vis[mxN];
+vector<int> topoSort;
+
+void dfs(int u) {
+	vis[u] = true;
+	for(auto v : adj[u]) {
+		if(!vis[v]) 
+			dfs(v);
+	}	
+	topoSort.push_back(u);
+}
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+	
+	while(M--) {
+		int u, v;
+		cin >> u >> v, --u, --v;
+		adj[u].push_back(v);
+	}
+	
+	for(int i = 0; i < N; ++i) {
+		if(!vis[i]) 
+			dfs(i);
+	}
+	
+	reverse(topoSort.begin(), topoSort.end());
+	
+	vector<int> dp(N, 0);
+	
+	int ans = 0;
+	
+	for(int i = 0; i < N; ++i) {
+		int u = topoSort[i];
+		for(auto v : adj[u]) {
+			dp[v] = max(dp[v], dp[u] + 1); 
+			ans = max(ans, dp[v]);
+		}
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
