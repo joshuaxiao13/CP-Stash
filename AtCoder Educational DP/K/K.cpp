b06fcdea2336704a0c97a381985e41fbf1be3ba6
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-15 12:14:59 -0500

    Create K.cpp

---
+++ AtCoder Educational DP/K/K.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int N, K;
+	cin >> N >> K;
+
+	vector<int> stones(N);
+
+	for(int&x : stones)
+		cin >> x;
+
+	vector<bool> dp(K + 1, false);
+
+	for(int i = 0; i < K; ++i) {
+		if(!dp[i]) {
+			for(auto x : stones)
+				if(i + x <= K) {
+					dp[i+x] = true;
+		}
+	}
+
+	cout << (dp[K] ? "First" : "Second") << '\n';
+
+	return 0;
+}
