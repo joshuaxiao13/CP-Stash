bd9118c0268ed12e04ce62d425c8a5684022ff64
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-17 12:16:44 -0500

    Update binaryIndexedTree.cpp

--- Algos/Range Queries/binaryIndexedTree.cpp
+++ Algos/Range Queries/binaryIndexedTree.cpp
@@ -8,6 +8,8 @@ const int mxN = 2e5;				// mxN = number of elements in original array
 int N, Q;					// Remember: Binary Indexed Trees are always 1-indexed
 ll tree[mxN + 1];				// long long to avoid integer overflow
 
+
+// returns sum of elements in the interval [1, k]
 ll sum(int k) {
 	ll ret = 0;
 	while(k >= 1) {
@@ -17,6 +19,7 @@ ll sum(int k) {
 	return ret;
 }
 
+// increases the kth element of the array by delta
 void add(int k, ll delta) {
 	while(k <= N) {
 		tree[k] += delta;
@@ -31,7 +34,7 @@ int main() {
 
 	cin >> N >> Q;
 	
-	// processing the elements in array and constructing the Fenwick Tree
+	// process elements in array and construct the Fenwick Tree
 	
 	for(int i = 1; i <= N; ++i) {
 		int x;
