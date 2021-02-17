b2417d9c5a565ad6660d1a1fb73ca5c63a32d100
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-17 10:39:44 -0500

    Create staticRangeMinimumQueries.cpp

---
+++ Algos/Range Queries/staticRangeMinimumQueries.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mxN = 2e5;	// mxN = maximum number of elements in array
+const int K = 17;		  // floor(log(2e5)/log(2)), in general K = floor(log(mxN)/log(2))
+
+int a[mxN];
+int st[mxN][K + 1];
+int LOG2[mxN + 1];
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int N, Q;
+	cin >> N >> Q;
+	
+	// https://cp-algorithms.com/data_structures/sparse-table.html
+	
+	LOG2[1] = 0;
+	
+	for(int i = 2; i <= mxN; ++i) {
+		LOG2[i] = LOG2[i/2] + 1;
+	}
+	
+	for(int i = 0; i < N; ++i) {
+		cin >> a[i];
+		st[i][0] = a[i];
+	}
+	
+	for(int j = 1; j <= K; ++j) {
+		for(int i = 0; i + (1<<j) - 1 < N; ++i) {
+			st[i][j] = min(st[i][j-1], st[i + (1<<(j-1))][j-1]);
+		}
+	}
+	
+	while(Q--) {
+		int L, R;
+		cin >> L >> R, --L, --R;
+		int j = LOG2[R - L + 1];
+		cout << min(st[L][j], st[R - (1<<j) + 1][j]) << '\n';
+	}
+	
+	return 0;
+}
