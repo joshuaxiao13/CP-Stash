57eea0bff00bfadcb6afef709f41ec6d1add52a9
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-19 13:35:36 -0500

    Create template.cpp

---
+++ Miscellaneous/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define pb push_back
+#define eb emplace_back
+#define f  first
+#define s  second
+
+using ll = long long;
+using vi = vector<int>;
+using indexed_set = tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>;
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	
+
+	return 0;
+}
