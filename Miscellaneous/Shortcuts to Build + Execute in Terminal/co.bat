6a29e7a9e8b4a9ab0c2c6edecfef3751c0fe2f81
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-18 21:41:55 -0500

    Update co.bat

--- Miscellaneous/Shortcuts to Build + Execute in Terminal/co.bat
+++ Miscellaneous/Shortcuts to Build + Execute in Terminal/co.bat
@@ -2,5 +2,9 @@
 
 echo [1] - Compiling	.....	%1
 g++ -std=c++0x %1.cpp -Wall -O2 -o %1.exe
-echo [2] - Compilation Successful ..... % 1
+
+if %errorlevel% == 0 (
+    echo [2] - Compilation Successful ..... %1
+    echo:
+)
 echo:
