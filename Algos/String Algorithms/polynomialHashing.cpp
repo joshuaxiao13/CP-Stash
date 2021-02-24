020ba45345d3beec5037905d2af69c2d4f3ca399
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-02-23 22:53:48 -0500

    Create polynomialHashing.cpp

---
+++ Algos/String Algorithms/polynomialHashing.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const ll P = 9973, M = 1e9 + 9;       // can change these constants
+
+ll p[1000001], hsh[1000001];          // p[i] = P^i, hsh[i] = hash value of the prefix s[0...i]
+string s, t;
+int n, m;                             //n = size of s, m = size of t
+
+void poly_hash() {
+	p[0] = 1;
+	hsh[0] = s[0];
+	for(int i = 1; i < n; ++i) {
+		p[i] = (p[i-1] * P) % M;
+		hsh[i] = ((hsh[i-1] * P) % M + s[i]) % M;
+	}
+}
+
+ll get_hash(int a, int b) {
+	if(a == 0) return hsh[b];                                     // to avoid a - 1 < 0 in the line below
+	return (hsh[b] - (hsh[a - 1] * p[b - a + 1]) % M + M) % M;    // add M because hsh[b] - (hsh[a - 1] * p[b - a + 1]) might be negative, we want hash values to be non-negative
+}
+
+int main() {
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	cin >> s >> t;
+	n = s.size();
+	m = t.size();
+	
+	poly_hash();
+	
+	ll hsh2 = 0;    // stores the hash value of string t
+  
+	for(int i = 0; i < m; ++i) {
+		(hsh2 += (t[i] * p[m - 1 - i]) % M) %= M;
+	}
+	
+	int cnt = 0;
+	
+	for(int i = 0; i + m - 1 < n; ++i) {
+		cnt += (get_hash(i, i + m - 1) == hsh2);
+	}
+	
+	cout << cnt << '\n';      // cnt = # of occurences of t in s
+	
+	return 0;
+}
