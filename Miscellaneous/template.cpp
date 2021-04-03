9247c8827d8ef9832da8256dd47e4f9782e94b85
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-04-02 21:52:04 -0400

    Update template.cpp

--- Miscellaneous/template.cpp
+++ Miscellaneous/template.cpp
@@ -12,10 +12,15 @@ using ll = long long;
 using vi = vector<int>;
 using indexed_set = tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>;
 
-int main() {
-	
+int main() { 
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
+
 	return 0;
 }
+
+/*
+* use long long when dealing with MOD 1e9 + 7
+* clear memory after testcases
+*/
+
