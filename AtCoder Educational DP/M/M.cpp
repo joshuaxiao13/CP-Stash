bf1776cd9f1f8eef1b6b46e3f633970658b85915
Joshua Xiao <66401883+joshuaxiao13@users.noreply.github.com>
2021-04-17 13:28:39 -0400

    Update M.cpp

--- AtCoder Educational DP/M/M.cpp
+++ AtCoder Educational DP/M/M.cpp
@@ -5,34 +5,32 @@ using namespace std;
 
 using ll = long long;
 
-const int MOD = 1e9 + 7;
+const int MOD = 1000000007;
 
 int main() {
-	
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	
-	int N, K;
-	cin >> N >> K;
-	
-	vector<int> a(N);
-	for(int&x : a)
-		cin >> x;
-	
-	vector<vector<ll>> dp(N + 1, vector<ll>(K + 1, 0));
-	
-	dp[0][K] = 1;
-	
-	for(int i = 1; i <= N; ++i) {
-		for(int R = 0; R <= K; ++R) {
-			int cnt = (dp[i-1][min(K, R + a[i-1])] - (R - 1 >= 0 ? dp[i-1][R-1] : 0)) % MOD;
-			if(cnt < 0) cnt += MOD;
-			(dp[i][R] += cnt) %= MOD;
-			if(R - 1 >= 0) (dp[i][R] += dp[i][R-1]) %= MOD;
-		}
-	}
-	
-	cout << dp[N][0] << '\n';
-  
-	return 0;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> a(N);
+    for(int& x : a) cin >> x;
+
+    vector<vector<ll>> dp(N + 1, vector<ll>(K + 1, 0));
+    dp[0][K] = 1;
+
+    for(int i = 1; i <= N; ++i) {
+        for(int rem = 0; rem <= K; ++rem) {
+            dp[i][rem] = dp[i-1][min(rem + a[i-1], K)] - (rem - 1 >= 0 ? dp[i-1][rem-1] : 0);
+            dp[i][rem] %= MOD;
+        }
+        for(int rem = 1; rem <= K; ++rem) (dp[i][rem] += dp[i][rem-1]) %= MOD;
+    }
+
+    cout << (dp[N][0] + MOD) % MOD << '\n';
+
+    return 0;
 }
+
